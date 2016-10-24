01446d13c4d33dc5fbc84591f5785036523976e3
danielRomero <daniel.romero.esteban@gmail.com>
2016-10-24 22:48:34 +0200

    testing to find products on store from checkout

--- test/lib/checkout_test.rb
+++ test/lib/checkout_test.rb
@@ -18,47 +18,52 @@ describe Checkout do
     before do
       Store.empty
       Store.setup_products 'test/resources/products_test.yml'
+      @checkout = Checkout.new(pricing_rules)
+    end
+
+    it 'cant find product on store' do
+      @checkout.scan('NOTFOUND').must_equal 'product not found'
+    end
+
+    it 'find product on store' do
+      @checkout.scan('VOUCHER').must_be_instance_of Product
     end
 
     it 'Items: VOUCHER, TSHIRT, MUG. Total: 32.50' do
-      co = Checkout.new(pricing_rules)
-      co.scan('VOUCHER')
-      co.scan('TSHIRT')
-      co.scan('MUG')
-      price = co.total
+      @checkout.scan('VOUCHER')
+      @checkout.scan('TSHIRT')
+      @checkout.scan('MUG')
+      price = @checkout.total
       assert_equal '32.50€', price
     end
 
     it 'Items: VOUCHER, TSHIRT, VOUCHER. Total: 25.00' do
-      co = Checkout.new(pricing_rules)
-      co.scan('VOUCHER')
-      co.scan('TSHIRT')
-      co.scan('VOUCHER')
-      price = co.total
+      @checkout.scan('VOUCHER')
+      @checkout.scan('TSHIRT')
+      @checkout.scan('VOUCHER')
+      price = @checkout.total
       assert_equal '25.00€', price
     end
 
     it 'Items: TSHIRT, TSHIRT, TSHIRT, VOUCHER, TSHIRT. Total: 81.00' do
-      co = Checkout.new(pricing_rules)
-      co.scan('TSHIRT')
-      co.scan('TSHIRT')
-      co.scan('TSHIRT')
-      co.scan('VOUCHER')
-      co.scan('TSHIRT')
-      price = co.total
+      @checkout.scan('TSHIRT')
+      @checkout.scan('TSHIRT')
+      @checkout.scan('TSHIRT')
+      @checkout.scan('VOUCHER')
+      @checkout.scan('TSHIRT')
+      price = @checkout.total
       assert_equal '81.00€', price
     end
 
     it 'Items: VOUCHER, TSHIRT, VOUCHER, VOUCHER, MUG, TSHIRT, TSHIRT. Total: 74.50' do
-      co = Checkout.new(pricing_rules)
-      co.scan('VOUCHER')
-      co.scan('TSHIRT')
-      co.scan('VOUCHER')
-      co.scan('VOUCHER')
-      co.scan('MUG')
-      co.scan('TSHIRT')
-      co.scan('TSHIRT')
-      price = co.total
+      @checkout.scan('VOUCHER')
+      @checkout.scan('TSHIRT')
+      @checkout.scan('VOUCHER')
+      @checkout.scan('VOUCHER')
+      @checkout.scan('MUG')
+      @checkout.scan('TSHIRT')
+      @checkout.scan('TSHIRT')
+      price = @checkout.total
       assert_equal '74.50€', price
     end
   end
